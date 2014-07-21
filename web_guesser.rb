f240156c1b3696d34dfccc53f1cb741929b75747
emilyadavis303 <emilyadavis303@gmail.com>
2014-07-21 16:10:02 -0600

    IT HAS COLOR BITCHES

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,19 +6,25 @@ NUMBER = rand(100)
 get '/' do
   guess = params['guess'].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  background = @color
+  erb :index, :locals => {:number => NUMBER, :message => message, :background => background}
 end
 
 def check_guess(guess)
   if guess > NUMBER + 5
+    @color = 'red'
     message = "Way too high!"
   elsif guess < NUMBER - 5 && params['guess']
+    @color = 'blue'
     message = "Way too low!"
   elsif guess < NUMBER && params['guess']
+    @color = 'cyan'
     message = "Too low!"
   elsif guess > NUMBER
+    @color = 'orange'
     message = "Too high!"
   elsif guess == NUMBER
+    @color = 'green'
     message = "You got it right!"
   end
 end
